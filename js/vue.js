dd62cca33ddbc5800793528ac558861a1a17d5c2
ARTEM DEMCHENKO <arteman8112@gmail.com>
2023-08-19 19:28:06 +0300

    upload

---
+++ js/vue.js
@@ -0,0 +1,9 @@
+function notFound() {
+    console.log(1221)
+    return (
+        `        <div class="not-found">
+        <img src="images/404.png" alt="">
+        <p>Not found 404</p>
+                </div>`
+    )
+}
